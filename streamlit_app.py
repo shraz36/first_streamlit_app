ce16d3e38eebc39453a5afd6e5daf93f69eb0533
shraz36 <71567674+shraz36@users.noreply.github.com>
2024-04-08 12:17:35 +0100

    Update streamlit_app.py
    new update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
-import streamlit
+import streamlit as st
+from snowflake.snowpark.functions import col
+import requests
+import pandas
 
-streamlit.title("My parents healthy diner")
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.write(
+    """ Choose the fruits you want in your custom Smoothie!
+    """)
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
