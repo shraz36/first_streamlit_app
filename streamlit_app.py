07d7ea307f5281eb5845d4b39b444955bd20a098
shraz36 <71567674+shraz36@users.noreply.github.com>
2024-04-08 14:30:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,21 +9,21 @@ my_cnx = st.connection("snowflake")
 my_cur = my_cnx.cursor()
 # run a snowflake query and put it all in a var called my_catalog
 my_cur.execute("select color_or_style from catalog_for_website")
-#my_catalog = my_cur.fetchall()
+my_catalog = my_cur.fetchall()
 # put the dafta into a dataframe
-#df = pandas.DataFrame(my_catalog)
+df = pandas.DataFrame(my_catalog)
 # temp write the dataframe to the page so I Can see what I am working with
-# streamlit.write(df)
+ streamlit.write(df)
 # put the first column into a list
-#color_list = df[0].values.tolist()
-# print(color_list)
+color_list = df[0].values.tolist()
+ print(color_list)
 # Let's put a pick list here so they can pick the color
-#option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
+option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
 # We'll build the image caption now, since we can
 #product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 # use the option selected to go back and get all the info from the database
 #my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where
-color_or_style" = '" + option + "';)
+#color_or_style" = '" + option + "';)
 #df2 = my_cur.fetchone()
 #streamlit.image(
 #df2[0],
